879c763f0b44f20c89c52ef534b0bfbc92bcad15
BJB2088 <BJB2088@gmail.com>
2015-04-22 13:29:18 -0700

    upds

---
+++ Supplement/bayesRoyle.R
@@ -0,0 +1,52 @@
+library(coda)
+library(lattice)
+library(rjags)
+library(R2jags)
+#sim test
+x=rpois(50,3)
+sum(x)
+mat=matrix(rep(x,6),nrow=50,ncol=6)
+n=matrix(rbinom(300,x,.5),nrow=50,ncol=6)
+
+# bird test
+n.it=t(birds)[8:57,]
+Date=matrix(rep(2003:2008,50),nrow=50,ncol=6,byrow=TRUE)
+
+model.3 <- function(){
+  for(i in 1:R){
+    for (m in 1:V){
+      y[i,m] ~ dbin(alpha.p, alpha.N[i])
+    }
+    alpha.N[i] ~ dpois(lambda)
+  }
+  
+  alpha.p ~ dunif(0,1)
+
+  lambda ~ dgamma(1.0E-6,1.0E-6)
+  }
+
+# --------------------
+# 3. fit the model
+# --------------------
+
+
+# Initial values
+
+#zst <- array(1,dim=c(Y,R))
+#ust <- array(1,dim=c(S,Y,R))
+
+n.it=n
+inits <- function(){list(alpha.p = runif(1,0,1),
+                         alpha.N = rep(8,50),
+                         lambda=3)}
+params <- c("alpha.p","lambda","alpha.N")
+test.data <- list(y = n.it, R = 50, V = 6)
+system.time(out3 <- jags(test.data, inits, params, model.3, n.chains = 3,
+                         n.thin = 50, n.iter = 50000, n.burnin = 1000))
+print(out3, 2)
+traceplot(out3)
+
+
+sum(out3$BUGSoutput[10]$summary[1:50,1])
+
+mean((as.vector(out3$BUGSoutput[10]$summary[1:50,1])-x)>1)
