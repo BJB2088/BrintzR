cf6e92efa759acec9de0411f8c838564a6dc8bad
BJB2088 <BJB2088@gmail.com>
2015-04-26 17:43:52 -0700

    upd

--- Supplement/bayesRoyle.R
+++ Supplement/bayesRoyle.R
@@ -42,7 +42,7 @@ inits <- function(){list(alpha.p = runif(1,0,1),
 params <- c("alpha.p","lambda","alpha.N")
 test.data <- list(y = n.it, R = nrow(n.it), V = ncol(n.it))
 system.time(out3 <- jags(test.data, inits, params, model.3, n.chains = 3,
-                         n.thin = 50, n.iter = 50000, n.burnin = 1000))
+                         n.thin = 20, n.iter = 20000, n.burnin = 1000))
 print(out3, 2)
 traceplot(out3)
 
@@ -68,21 +68,28 @@ model.3 <- function(){
   }
   
   p.int ~ dunif(0,1)
-  p.cov.2004 ~ dunif(0,1)
-  p.cov.2005 ~ dunif(0,1)
-  p.cov.2006 ~ dunif(0,1)
-  p.cov.2007 ~ dunif(0,1)
-  p.cov.2008 ~ dunif(0,1)
+  p.cov.2004 ~ dnorm(0,1)
+  p.cov.2005 ~ dnorm(0,1)
+  p.cov.2006 ~ dnorm(0,1)
+  p.cov.2007 ~ dnorm(0,1)
+  p.cov.2008 ~ dnorm(0,1)
   
   lambda ~ dgamma(1.0E-6,1.0E-6)
 }
 
 inits <- function(){list(
                          alpha.N = rep(8,nrow(n.it)),
-                         lambda=3)}
+                         lambda=3,
+                         p.int=.5, 
+                         p.cov.2004=.1,
+                         p.cov.2005=.1,
+                         p.cov.2006=.1,
+                         p.cov.2007=.1,
+                         p.cov.2008=.1
+                         )}
 params <- c("p.int","p.cov.2004","p.cov.2005","p.cov.2006",
             "p.cov.2007","p.cov.2008","lambda","alpha.N")
-test.data <- list(y = n.it, R = nrow(n.it), V = ncol(n.it),Date=Datevec)
+test.data <- list(y = n.it, R = nrow(n.it), V = ncol(n.it),Date=Datevec[1,])
 system.time(out3 <- jags(test.data, inits, params, model.3, n.chains = 3,
                          n.thin = 50, n.iter = 30000, n.burnin = 1000))
 print(out3, 2)
@@ -90,3 +97,39 @@ traceplot(out3)
 
 
 sum(out3$BUGSoutput[10]$summary[1:50,1])
+
+# Dave's Model
+model.3 <- function(){
+  for(i in 1:R){
+    y[i,1] ~ dbin(alpha.p, alpha.N[i,1])
+    alpha.N[i,1] ~ dpois(lambda)
+    for (m in 2:V){
+      y[i,m] ~ dbin(alpha.p, alpha.N[i,m])
+      alpha.N[i,m] ~ dpois(lambda)
+    }
+  }
+  
+  alpha.p ~ dunif(0,1)
+  
+  lambda ~ dgamma(1.0E-6,1.0E-6)
+}
+
+# --------------------
+# 3. fit the model
+# --------------------
+
+
+# Initial values
+
+#zst <- array(1,dim=c(Y,R))
+#ust <- array(1,dim=c(S,Y,R))
+
+n.it=n
+inits <- function(){list(alpha.p = runif(1,0,1),
+                         alpha.N = rep(8,nrow(n.it)),
+                         lambda=3)}
+params <- c("alpha.p","lambda","alpha.N")
+test.data <- list(y = n.it, R = nrow(n.it), V = ncol(n.it))
+system.time(out3 <- jags(test.data, inits, params, model.3, n.chains = 3,
+                         n.thin = 20, n.iter = 20000, n.burnin = 1000))
+print(out3, 2)
